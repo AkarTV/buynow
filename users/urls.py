de12377fd590a7aae6a7683bb1bc92a0921da2e0
AkarTV <AkarTV@github.com>
2021-11-09 10:23:17 +0300

    Added the user profile's editing backend

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('registration/', views.register, name='registration')
+    path('registration/', views.register, name='registration'),
+    path('profile/', views.profile_page, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
 ] 
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserForm, CustomerForm
 
@@ -21,3 +22,23 @@ def register(request):
             return redirect('main')
     context = {'form': base_form, 'extend_form': extend_form, 'custom_form': custom_form}
     return render(request, 'registration/registration.html', context)
+
+def profile_page(request):
+    '''Return the page of the user profile'''
+    current_user = request.user
+    return render(request, 'registration/profile_page.html', {'user': current_user})
+
+def edit_profile(request):
+    '''Return the user profie editing page'''
+    if request.method != 'POST':
+        extend_form = UserForm(instance=request.user)
+        custom_form = CustomerForm(instance=request.user.customer)
+    else:
+        extend_form = UserForm(data=request.POST)
+        custom_form = CustomerForm(data=request.POST)
+        if extend_form.is_valid() and custom_form.is_valid():
+            extend_form.save()
+            custom_form.save()
+            return redirect('profile')
+    context = {'extend_form': extend_form, 'custom_form': custom_form}
+    return render(request, 'registration/edit_profile.html', context)
